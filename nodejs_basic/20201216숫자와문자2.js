dcc9237773d68339237338da515f45c14d20f3ae
n_snow <n990220@gmail.com>
2020-12-16 21:51:15 +0900

    20201216-4

---
+++ nodejs_basic/20201216숫자와문자2.js
@@ -0,0 +1,34 @@
+console.log(
+    
+    Math.pow(3,2),
+    /// 제곱
+    /// 여기서 . 은 객체라는 개념으로 나중에 할 것 
+    /// Math라는 것 안에 pow라는 명령이 있음
+
+    Math.round(10.6),
+    /// 반올림 
+
+    Math.ceil(10.2),
+    /// 정수로 올림 
+
+    Math.floor(10.2),
+    /// 정수로 내림 
+
+    Math.sqrt(9),
+    /// 제곱근 
+
+    Math.random(),
+    /// 랜덤한 숫자 
+
+    100 * Math.random(),
+    /// 100 이하의 임의의 수 
+    /// random은 1 이하의 랜덤 수 
+
+    Math.round(100 * Math.random())
+
+);
+
+console.log('박지현 바보') ;
+console.log('이준엽 멍청이') ;
+
+ 
