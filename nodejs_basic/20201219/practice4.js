7ab424b03d3f3ed34d16a857f1d6c2dc00ff7e5c
n_snow <n990220@gmail.com>
2020-12-20 17:26:06 +0900

    20201220-2

--- nodejs_basic/20201219/practice4.js
+++ nodejs_basic/20201219/practice4.js
@@ -9,3 +9,32 @@ function number2(arg1, arg2) {
 }
 
 console.log(number2(10, 20));
+
+function hnam(arg) {
+  return "배통통" + arg + "번!";
+}
+
+for (arg = 0; arg < 10; arg = arg + 1) {
+  console.log(hnam(arg));
+}
+
+var t = prompt("배통통 몇 번?");
+
+for (i = 1; i < t; i = i + 1) {
+  document.write(i + "통통!" + "<br />");
+}
+
+var id = prompt("put your id");
+
+if (id === "ilsun") {
+  alert("id success");
+  var password = prompt("put your password");
+
+  if (password === "111111") {
+    alert("login success");
+  } else {
+    alert("login fail");
+  }
+} else {
+  alert("login fail");
+}
