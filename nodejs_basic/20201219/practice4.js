886e7490a9913a858c37578865099864c8a62cf1
n_snow <n990220@gmail.com>
2020-12-27 18:42:05 +0900

    20201227-4

--- nodejs_basic/20201219/practice4.js
+++ nodejs_basic/20201219/practice4.js
@@ -11,17 +11,17 @@ function number2(arg1, arg2) {
 console.log(number2(10, 20));
 
 function hnam(arg) {
-  return "배통통" + arg + "번!";
+  return "안녕" + arg + "번!";
 }
 
 for (arg = 0; arg < 10; arg = arg + 1) {
   console.log(hnam(arg));
 }
 
-var t = prompt("배통통 몇 번?");
+var t = prompt("안녕 몇 번?");
 
 for (i = 1; i < t; i = i + 1) {
-  document.write(i + "통통!" + "<br />");
+  document.write(i + "안녕!" + "<br />");
 }
 
 var id = prompt("put your id");
--- nodejs_webapp/20201227/os.js
+++ nodejs_webapp/20201227/os.js
@@ -1,2 +1,24 @@
-var o = require('os');
+/*const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 1337;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  }); */
+
+//* 모듈: 부품이라고 생각
+// nodejs가 이미 마련한 웹서버를 가져다 쓰는 방법
+// require: 모듈을 불러오는 것, 리턴값을 불러와서 http에 담은 것
+// constant: 상수
+// http라는 모듈에 속한 server라는 객체를 리턴한다.
+
+var o = require("os");
 console.log(o.platform());
+// os라는 모듈은 platform이라는 메소드를 가진다.
+// require라는 함수는 os 모듈을 리턴한다.
