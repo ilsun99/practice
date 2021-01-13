f614444ddbf6495960be4676068b5ea6984c7f81
n_snow <n990220@gmail.com>
2021-01-13 21:38:18 +0900

    20200113-1

--- 3_web2/main.js
+++ 3_web2/main.js
@@ -1,16 +1,49 @@
 var http = require("http");
 var fs = require("fs");
-var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
-  }
-  if (request.url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
+var url = require("url");
+
+var app = http.createServer(function (req, res) {
+  var _url = req.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+  // console.log(queryData.id); id 값을 받아옴
+
+  if (url == "/favicon.ico") {
+    res.writeHead(404);
+    res.end();
     return;
   }
-  response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
+
+  if (_url == "/") {
+    var title = "welcome";
+  }
+
+  res.writeHead(200);
+
+  fs.readFile(`data/${queryData.id}`, "utf8", function (err, data) {
+    var description = data;
+    var template = `
+  <!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ul>
+    <li><a href="/?id=html">HTML</a></li>
+    <li><a href="/?id=css">CSS</a></li>
+    <li><a href="/?id=javascript">Javascript</a></li>
+  </ul>
+  <h2>${title}</h2>
+  <p>${description}</p>
+</body>
+</html>`;
+    res.end(template);
+  });
 });
-app.listen(3000);
+
+if (app.listen(3000)) {
+  console.log("listening to port 3000");
+}
