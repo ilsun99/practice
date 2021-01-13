20a50af78382e938b059270275b84ac86b5b4ce5
n_snow <n990220@gmail.com>
2021-01-13 22:08:56 +0900

    20200113-3

--- 3_web2/main.js
+++ 3_web2/main.js
@@ -5,43 +5,38 @@ var url = require("url");
 var app = http.createServer(function (req, res) {
   var _url = req.url;
   var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
   // console.log(queryData.id); id 값을 받아옴
 
-  if (url == "/favicon.ico") {
-    res.writeHead(404);
-    res.end();
-    return;
+  if (pathname === "/") {
+    fs.readFile(`data/${queryData.id}`, "utf8", function (err, data) {
+      var description = data;
+      var template = `
+    <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>
+      <li><a href="/?id=html">HTML</a></li>
+      <li><a href="/?id=css">CSS</a></li>
+      <li><a href="/?id=javascript">Javascript</a></li>
+    </ul>
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>`;
+      res.writeHead(200);
+      res.end(template);
+    });
+  } else {
+    res.writeHead(200);
+    res.end("not found");
   }
-
-  if (_url == "/") {
-    var title = "welcome";
-  }
-
-  res.writeHead(200);
-
-  fs.readFile(`data/${queryData.id}`, "utf8", function (err, data) {
-    var description = data;
-    var template = `
-  <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ul>
-    <li><a href="/?id=html">HTML</a></li>
-    <li><a href="/?id=css">CSS</a></li>
-    <li><a href="/?id=javascript">Javascript</a></li>
-  </ul>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>`;
-    res.end(template);
-  });
 });
 
 if (app.listen(3000)) {
