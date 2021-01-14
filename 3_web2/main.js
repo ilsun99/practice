7527d310f8a1c9c7a39c262922bb6269d02f396b
n_snow <n990220@gmail.com>
2021-01-14 15:36:01 +0900

    20200114-1

--- 3_web2/main.js
+++ 3_web2/main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+// 여기서 url은 모듈을 받아온 것
 
 var app = http.createServer(function (req, res) {
   var _url = req.url;
@@ -8,6 +9,9 @@ var app = http.createServer(function (req, res) {
   var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
   // console.log(queryData.id); id 값을 받아옴
+  // _url은 요청받은 url이라는 뜻
+  // url.parse는 url문자열을 url객체로 반환해준다.
+  // true일 경우 .query, .pathname을 객체로 반환한다.
 
   if (pathname === "/") {
     fs.readFile(`data/${queryData.id}`, "utf8", function (err, data) {
@@ -35,9 +39,13 @@ var app = http.createServer(function (req, res) {
     });
   } else {
     res.writeHead(200);
-    res.end("not found");
+    res.end("cannot find");
   }
 });
+// res.writeHead: res를 헤더에 담아 내보낸다.
+// write: 바디 부분, 보낼 것이 더 있으면 여러 번 작성 가능
+// res.end: 응답 종료. 인수값을 내보낸 후 콘텐츠 응답을 완료한다.
+// -> 이 세개로 요청에 응답하는 것
 
 if (app.listen(3000)) {
   console.log("listening to port 3000");
